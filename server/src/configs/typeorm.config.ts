abe276725c6cb9403e738be37fb006302e9acaee
ppogulailo <pavel.pogulailo@gmail.com>
2024-08-06 20:10:40 +0300

    server init

---
+++ server/src/configs/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default (configService: ConfigService): TypeOrmModuleOptions => ({
+  type: configService.get<'postgres'>('TYPEORM_TYPE'),
+  host: configService.get('TYPEORM_HOST'),
+  port: configService.get('TYPEORM_PORT'),
+  username: configService.get('TYPEORM_USERNAME'),
+  password: configService.get('TYPEORM_PASSWORD'),
+  database: configService.get<string>('TYPEORM_DATABASE'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: false,
+});
